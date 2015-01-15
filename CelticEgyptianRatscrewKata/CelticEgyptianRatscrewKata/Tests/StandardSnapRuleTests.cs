f8f8969505bddaeedffcc59c3ebef6864d6a2539
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-15 18:32:40 +0000

    Red: failing test for standard snap with no pairs

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapRuleTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.SnapRules;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class StandardSnapRuleTests
+    {
+        [Test]
+        public void ShouldFailIfNoPairFound()
+        {
+            var stack = new Stack(new List<Card>
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Clubs, Rank.Two),
+            });
+
+            var rule = new StandardSnapRule();
+            Assert.That(rule.CanSnap(stack), Is.False);
+
+        }
+    }
+}
