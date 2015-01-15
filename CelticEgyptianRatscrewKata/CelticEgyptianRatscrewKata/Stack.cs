68595b11f45dfe426d097e9d0f9f5a710ecbfb06
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-15 17:29:24 +0000

    Green: Implement stack to make test pass

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Stack.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Stack.cs
@@ -1,7 +1,25 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CelticEgyptianRatscrewKata
 {
-    public class Stack
+    public class Stack : IEnumerable<Card>
     {
-        
+        private readonly List<Card> m_Cards;
+
+        public Stack(IEnumerable<Card> cards)
+        {
+            m_Cards = new List<Card>(cards);
+        }
+
+        public IEnumerator<Card> GetEnumerator()
+        {
+            return m_Cards.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
