84761866a82067daa3dd4bf8967048ef4c143c74
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-15 15:03:02 +0000

    Red: Add first Stack test

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StackTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StackTests.cs
@@ -1,13 +1,19 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
 {
     public class StackTests
     {
         [Test]
-        public void ShouldReadCardsInStack()
+        public void ShouldReadTopCardOnStack()
         {
-            
+            var expectedCard = new Card(Suit.Clubs, Rank.Ace);
+            var stack = new Stack(new List<Card> {expectedCard});
+            var card = stack.First();
+
+            Assert.That(card, Is.EqualTo(expectedCard));
         }
     }
 }
