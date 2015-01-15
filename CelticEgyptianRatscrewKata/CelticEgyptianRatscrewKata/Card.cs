ba2ad1e20f72d05da7fe3e555c5d69e219684ed1
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-15 15:00:16 +0000

    R# implement equality members

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Card.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Card.cs
@@ -2,8 +2,36 @@
 {
     public class Card
     {
+        private readonly Suit m_Suit;
+        private readonly Rank m_Rank;
+
         public Card(Suit suit, Rank rank)
         {
+            m_Suit = suit;
+            m_Rank = rank;
         }
+
+#region EqualityMembers
+        protected bool Equals(Card other)
+        {
+            return m_Suit == other.m_Suit && m_Rank == other.m_Rank;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Card) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((int) m_Suit*397) ^ (int) m_Rank;
+            }
+        }
+#endregion
     }
 }
