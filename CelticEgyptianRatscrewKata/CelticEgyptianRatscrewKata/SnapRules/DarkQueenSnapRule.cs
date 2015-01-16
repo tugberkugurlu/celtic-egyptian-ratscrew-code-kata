7a476eb023633bf66a9f7d953b95a8ce701107b2
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-16 11:28:30 +0000

    R# extract field

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/DarkQueenSnapRule.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapRules/DarkQueenSnapRule.cs
@@ -4,10 +4,12 @@ namespace CelticEgyptianRatscrewKata.SnapRules
 {
     public class DarkQueenSnapRule : IRule
     {
+        private static readonly Card s_QueenOfSpades = new Card(Suit.Spades, Rank.Queen);
+
         public bool CanSnap(Stack stack)
         {
             var topCard = stack.FirstOrDefault();
-            return topCard != null && topCard.Equals(new Card(Suit.Spades, Rank.Queen));
+            return topCard != null && topCard.Equals(s_QueenOfSpades);
         }
     }
 }
