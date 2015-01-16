72930f0ccabc2c2f834aceb89d203cd3ba26e1c5
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-16 11:34:39 +0000

    Red: sandwich snap on empty stack

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class SandwichSnapRuleTests
+    {
+        [Test]
+        public void ShouldFailOnEmptyStack()
+        {
+            var rule = new SandwichSnapRule();
+            var stack = Stack.Empty();
+            Assert.That(rule.CanSnap(stack), Is.False);
+        }
+    }
+
+    public class SandwichSnapRule : IRule
+    {
+        public bool CanSnap(Stack stack)
+        {
+            return true;
+        }
+    }
+}
