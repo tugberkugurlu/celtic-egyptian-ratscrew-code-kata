58506e919061111bcc469f8be06517b0f4a26f6b
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-16 11:42:03 +0000

    Green: passing test with invalid stack

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
@@ -1,4 +1,5 @@
-﻿using CelticEgyptianRatscrewKata.SnapRules;
+﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.SnapRules;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
@@ -10,6 +11,19 @@ namespace CelticEgyptianRatscrewKata.Tests
         {
             var rule = new SandwichSnapRule();
             var stack = Stack.Empty();
+            Assert.That(rule.CanSnap(stack), Is.False);
+        }
+
+        [Test]
+        public void ShouldFailWithSandwichInStack()
+        {
+            var rule = new SandwichSnapRule();
+            var stack = new Stack(new List<Card>
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Clubs, Rank.Two),
+                new Card(Suit.Clubs, Rank.Three),
+            });
             Assert.That(rule.CanSnap(stack), Is.False);
         }
     }
