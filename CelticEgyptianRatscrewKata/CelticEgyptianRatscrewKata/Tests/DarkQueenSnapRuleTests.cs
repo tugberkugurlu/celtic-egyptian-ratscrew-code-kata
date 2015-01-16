a9138cd869c30da1c7804bffd858a3324b1db1cf
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-16 11:20:53 +0000

    Passing tests for queen not at top

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenSnapRuleTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenSnapRuleTests.cs
@@ -1,4 +1,5 @@
-﻿using CelticEgyptianRatscrewKata.SnapRules;
+﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.SnapRules;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
@@ -12,5 +13,29 @@ namespace CelticEgyptianRatscrewKata.Tests
             var stack = Stack.Empty();
             Assert.That(rule.CanSnap(stack), Is.False);
         }
+
+        [Test]
+        public void ShouldFailWithQueenNotAtTop()
+        {
+            var rule = new DarkQueenSnapRule();
+            var stack = new Stack(new List<Card>
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+                new Card(Suit.Spades, Rank.Queen)
+            });
+            Assert.That(rule.CanSnap(stack), Is.False);
+        }
+
+        [Test]
+        public void ShouldFailWithNoQueenOfSpades()
+        {
+            var rule = new DarkQueenSnapRule();
+            var stack = new Stack(new List<Card>
+            {
+                new Card(Suit.Clubs, Rank.Ace),
+            });
+            Assert.That(rule.CanSnap(stack), Is.False);
+        }
+
     }
 }
