284727eec30665a686d872d0736e0ea78d481d8c
Luke Drury <luke.drury@red-gate.com>
2015-01-30 16:35:13 +0000

    Red: shuffling a deck of one card

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/ShufflerTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/ShufflerTests.cs
@@ -15,5 +15,22 @@ namespace CelticEgyptianRatscrewKata.Tests
             var expectedDeck = Cards.Empty();
             Assert.That(shuffledDeck, Is.EqualTo(expectedDeck));
         }
+
+        [Test]
+        public void ShufflingDeckWithOneCardReturnsDeckWithOneCard()
+        {
+            var deck = Cards.With(AceOfClubs());
+            var shuffler = new Shuffler();
+
+            var shuffledDeck = shuffler.Shuffle(deck);
+
+            var expectedDeck = Cards.With(AceOfClubs());
+            Assert.That(shuffledDeck, Is.EqualTo(expectedDeck));
+        }
+
+        private static Card AceOfClubs()
+        {
+            return new Card(Suit.Clubs, Rank.Ace);
+        }
     }
 }
