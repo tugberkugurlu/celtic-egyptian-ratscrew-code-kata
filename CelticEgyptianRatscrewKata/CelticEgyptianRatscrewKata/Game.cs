7559713faab2ee8c53d25f4e9520609b018b961a
Tugberk Ugurlu <tugberk@outlook.com>
2015-02-09 13:04:16 +0000

    Add Game class

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelticEgyptianRatscrewKata
+{
+    public class Game
+    {
+        private readonly IList<Player> _playersList;
+
+        public Game(IEnumerable<Player> players)
+        {
+            if (players == null)
+            {
+                throw new ArgumentNullException("players");
+            }
+
+            _playersList = players.ToList();
+            if (_playersList.Count < 2)
+            {
+                throw new ArgumentException("Game cannot be played with less than 2 players.", "players");
+            }
+        }
+    }
+
+    public class Player
+    {
+        private readonly string _name;
+
+        public Player(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class GameTests
+    {
+        [Test]
+        public void Game_Ctor_Should_Throw_ArgumentException_If_Players_Count_Is_Less_Than_2()
+        {
+            var players = new[] {new Player("Mark")};
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => new Game(players));
+            Assert.AreEqual("players", exception.ParamName);
+        }
+    }
+}
