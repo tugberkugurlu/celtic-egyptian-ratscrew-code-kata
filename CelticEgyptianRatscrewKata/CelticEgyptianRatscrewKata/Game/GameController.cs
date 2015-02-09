bc9cf2dfdb5e846104c1187ab1443886d6aa5080
Peter Gerrard <32jezza@gmail.com>
2015-02-09 14:58:55 +0000

    GameController with AddPlayer method.
    Also create IPlayer interface.

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CelticEgyptianRatscrewKata.SnapRules;
+
+namespace CelticEgyptianRatscrewKata.Game
+{
+    /// <summary>
+    /// Controls a game of Celtic Egyptian Ratscrew.
+    /// </summary>
+    public class GameController
+    {
+        private readonly SnapValidator m_SnapValidator;
+        private readonly IList<IPlayer> m_Players;
+        private readonly IEnumerable<IRule> m_Rules;
+        private readonly GameState m_GameState;
+
+        public GameController(SnapValidator snapValidator, IEnumerable<IRule> rules)
+        {
+            m_Players = new List<IPlayer>();
+            m_GameState = new GameState();
+            m_SnapValidator = snapValidator;
+            m_Rules = rules;
+        }
+
+        public bool AddPlayer(IPlayer player)
+        {
+            if (m_Players.Any(x => x.Name == player.Name)) return false;
+
+            m_Players.Add(player);
+            m_GameState.AddPlayer(player.Name, Cards.Empty());
+            return true;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace CelticEgyptianRatscrewKata.Game
+{
+    /// <summary>
+    /// Represents a player of the game.
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// The name of the player, <em>must</em> be unique.
+        /// </summary>
+        string Name { get; } 
+    }
+}
