908501626b16b2cff438b58a565fdac75ecef752
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-02-13 16:15:55 +0000

    Add GameFactory and use it in the console app

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/GameFactory.cs
@@ -0,0 +1,19 @@
+﻿using CelticEgyptianRatscrewKata.GameSetup;
+using CelticEgyptianRatscrewKata.SnapRules;
+
+namespace CelticEgyptianRatscrewKata.Game
+{
+    public class GameFactory
+    {
+        public GameController Create()
+        {
+            IRule[] rules =
+            {
+                new DarkQueenSnapRule(),
+                new SandwichSnapRule(),
+                new StandardSnapRule(),
+            };
+            return new GameController(new GameState(), new SnapValidator(rules), new Dealer(), new Shuffler());
+        }
+    }
+}
