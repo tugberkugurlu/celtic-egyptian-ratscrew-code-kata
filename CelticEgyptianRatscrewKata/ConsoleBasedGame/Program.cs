46055ec6df54ba21c24c98b608ea5f97a9c13d6a
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-02-13 17:27:05 +0000

    Remove redundant using statments

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using CelticEgyptianRatscrewKata;
-using CelticEgyptianRatscrewKata.Game;
+﻿using CelticEgyptianRatscrewKata.Game;
 
 namespace ConsoleBasedGame
 {
